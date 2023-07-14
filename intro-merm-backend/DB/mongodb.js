e3ae7eaa48aa3207dcbf93a834960ab6b437afb3
Marketing2 Plaginsa <marketing2plaginsa@iMac-de-Marketing2.local>
2023-07-14 18:13:22 +0200

    nos vamos

--- intro-merm-backend/DB/mongodb.js
+++ intro-merm-backend/DB/mongodb.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 
 async function connectDB ({host,port,dbName}){
     const uri = `mongodb://${host}:${port}/${dbName}`
-    mongoose.connect(uri, {userNewUrlParser:true})
+    mongoose.connect(uri, {useNewUrlParser:true})
 }
 
 module.exports =connectDB;
--- intro-merm-backend/server.js
+++ intro-merm-backend/server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
+
 const express = require('express');
 const connectDB = require('./DB/mongodb');
-const { appConfig } = require('./config');
+const { appConfig, db } = require('./config');
 
 const app = express();
+
+connectDB(db);
 
 app.listen(appConfig.port, () => console.log(`Listening on ${appConfig.port}`));
 

86649d56a83c39b2dc1219e2319698d41c8cc05a
Marketing2 Plaginsa <marketing2plaginsa@iMac-de-Marketing2.local>
2023-07-17 10:37:16 +0200

    configuracion y estructura mongoDB

--- intro-merm-backend/DB/mongodb.js
+++ intro-merm-backend/DB/mongodb.js
@@ -1,5 +1,7 @@
 const mongoose =require('mongoose');
 
+mongoose.connection.on('open', () => console.log('db connected'))
+
 async function connectDB ({host,port,dbName}){
     const uri = `mongodb://${host}:${port}/${dbName}`
     mongoose.connect(uri, {useNewUrlParser:true})

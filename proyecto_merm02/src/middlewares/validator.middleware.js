3744d7f443f162f5ac6a8f877b288b252ec07996
Luismi <mm84alonso@gmail.com>
2023-08-04 13:04:31 +0200

    proyectos merm 02 backend full

--- proyecto_merm02/src/middlewares/validator.middleware.js
+++ proyecto_merm02/src/middlewares/validator.middleware.js
@@ -2,7 +2,7 @@ export const validateSchema = (schema) => (req, res, next) => {
     try{
         schema.parse(req.body)
         next()
-    }catch(err){
-        return res.status(400).json({err})
+    }catch(error){
+        return res.status(400).json({error: error.errors.map((error) => error.message ) })
     }
 }
--- proyecto_merm02/src/routes/tasks.routhes.js
+++ proyecto_merm02/src/routes/tasks.routhes.js
@@ -6,14 +6,16 @@ import {
     getTasks,
     deleteTask,
     updateTask} from '../controllers/tasks.controller.js'
+import { validateSchema } from '../middlewares/validator.middleware.js'
+import { taskSchema } from '../schemas/task.schema.js'
 
 const taskRouter = Router()
 
 taskRouter.get('/tasks',authRequire,getTasks)
 taskRouter.get('/tasks/:id',authRequire,getTask)
-taskRouter.post('/tasks',authRequire,createTask)
+taskRouter.post('/tasks',authRequire,validateSchema(taskSchema),createTask)
 taskRouter.delete('/tasks/:id',authRequire,deleteTask)
-taskRouter.put('/tasks/:id',authRequire,updateTask)
+taskRouter.put('/tasks/:id',authRequire,validateSchema(taskSchema),updateTask)
 
 
 export default taskRouter

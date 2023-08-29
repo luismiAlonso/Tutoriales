4d67fa336c8073bb43e024b5c6d974139db86bfe
Luismi <mm84alonso@gmail.com>
2023-08-29 15:36:19 +0200

    cliente antes de añadir credenciales para cookies

--- proyecto_merm02/client/src/pages/LoginPage.jsx
+++ proyecto_merm02/client/src/pages/LoginPage.jsx
@@ -1,49 +1,57 @@
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../context/authContext'
 
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+  const { singIn, errors: loginErrors } = useAuth()
+  const onSubmit = handleSubmit((data) => {
+    singIn(data)
+  })
 
-    const { register, handleSubmit, formState: { errors } } = useForm()
-    const { singIn, errors: loginErrors } = useAuth()
-    const onSubmit = handleSubmit(data => {
-        singIn(data)
-    })
-
-    return (
-        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
-            {
-                loginErrors.map((error, i) => (
-                    <div className="bg-red-500 p-2 text-white my-2" key={i}>
-                        {error}
-                    </div>
-                ))
-            }
-            <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
-                <h1 className="text-2xl font-bold">Login</h1>
-                <form onSubmit={onSubmit} >
-                    <input type="email" name="email" {...register('email', { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                        placeholder="Email" />
-                    {
-                        errors.email && (
-                            <p className="text-red-500">Email is required</p>
-                        )}
-                    <input type="password" name="password" {...register('password', { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                        placeholder="password" />
-                    {
-                        errors.password && (
-                            <p className="text-red-500">Password is required</p>
-                        )}
-                    <button type="submit">Login</button>
-                </form>
-                <p className="flex gap-x-2 justify-between">
-                    Don't have an account? <Link className="text-sky-500" to="/register">Sing Up</Link>
-                </p>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        {loginErrors.map((error, i) => (
+          <div className="bg-red-500 p-2 text-white my-2" key={i}>
+            {error}
+          </div>
+        ))}
+        <h1 className="text-2xl font-bold">Login</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            name="email"
+            {...register('email', { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Email"
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+          <input
+            type="password"
+            name="password"
+            {...register('password', { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="password"
+          />
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          <button type="submit">Login</button>
+        </form>
+        <p className="flex gap-x-2 justify-between">
+          Don't have an account?{' '}
+          <Link className="text-sky-500" to="/register">
+            Sing Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default LoginPage;
+export default LoginPage

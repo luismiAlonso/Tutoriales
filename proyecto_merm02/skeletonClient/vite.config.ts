223151ad767edd5870978d78e49ca71213958f16
Luismi <mm84alonso@gmail.com>
2024-02-05 09:49:56 +0100

    prev servidor

--- proyecto_merm02/skeletonClient/vite.config.ts
+++ proyecto_merm02/skeletonClient/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../compilePlaginsa', // Especifica aquí el nombre de la carpeta de salida
+  },
 })
